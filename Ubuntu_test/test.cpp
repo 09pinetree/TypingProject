e8cc66d1783cefc6e7dc3a47a1b3b0e4999453a6
root <root@DESKTOP-PCHF28H.localdomain>
2020-12-08 23:11:53 +0900

    draw encapsulation

--- Ubuntu_test/test.cpp
+++ Ubuntu_test/test.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 #include<string>
 #include<ctime>
+#include "Draw.h"
 #pragma comment(lib, "winmm.lib")
 using namespace std;
 
@@ -50,108 +51,13 @@ enum KEYBOARD
 	Pos.Y = y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }*/
-
-void gotoxy(int x, int y) {
-    printf("\033[%d;%df", y, x);
-    fflush(stdout);
-}
-
-
-//title, console size
 void SetConsoleView()
 {
-//	system("mode con:cols=50 lines=20");
-//	system("title DanceDance");
+	//	system("mode con:cols=50 lines=20");
+	//	system("title DanceDance");
 }
 
-//-----------Draw-----------------
-void DrawReadyGame()
-{
-	system("clear");
-	gotoxy(5, 2);
-	cout << "******************************";
-	gotoxy(5, 3);
-	cout << "*        Dance Dance!        *";
-	gotoxy(5, 4);
-	cout << "******************************";
-	gotoxy(10, 8);
-	cout << "GameStart";
-	gotoxy(10, 9);
-	cout << "GameInfo";
-	gotoxy(10, 10);
-	cout << "Quit" << endl;
-}
-
-void DrawInfoGame()
-{
-	system("clear");
-	gotoxy(1, 3);
-	cout << "*******************************************";
-	gotoxy(1, 4);
-	cout << "|Developer - BlockDMask";
-	gotoxy(1, 5);
-	cout << "|Blog - https://blockdmask.tistory.com/";
-	gotoxy(1, 8);
-	cout << "|Thank you.";
-	gotoxy(1, 9);
-	cout << "*******************************************";
-	gotoxy(1, 10);
-	cout << "press SPACE! to return to main menu" <<endl;
-}
-
-void DrawStartGame(const int life, const int score, const string questionStr, const string answerStr)
-{
-	system("clear");
-	gotoxy(2, 1);
-	cout << "*******************************************";
-	gotoxy(4, 3);
-	cout << "Life : " << life << " / " << LIFE;
-	gotoxy(4, 4);
-	cout << "Score : " << score;
-	gotoxy(4, 8);
-	cout << "Q : " << questionStr;
-	gotoxy(4, 10);
-	cout << "A : " << answerStr;
-	gotoxy(4, 12);
-	cout << "press SPACE! after input done.";
-	gotoxy(2, 18);
-	cout << "*******************************************" << endl;
-}
-
-void DrawGameOver(const int playTime)
-{
-	system("clear");
-	gotoxy(2, 1);
-	cout << "*******************************************";
-	gotoxy(8, 8);
-	cout << "-------------------";
-	gotoxy(8, 9);
-	cout << "| G A M E O V E R |";
-	gotoxy(8, 10);
-	cout << " " << playTime << " sec";
-	gotoxy(8, 11);
-	cout << "-------------------";
-	gotoxy(4, 12);
-	cout << "press SPACE! to return to main menu";
-	gotoxy(2, 18);
-	cout << "*******************************************" <<endl;
-}
-
-void DrawUserCursor(int& y)
-{
-	if (y <= 0)
-	{
-		y = 0;
-	}
-	else if (y >= 2)
-	{
-		y = 2;
-	}
-
-	gotoxy(9, 8 + y);
-	cout << ">";
-}
-
+Draw draw;
 //-----------Func-----------------
 MENU ReadyGame()
 {
@@ -159,8 +65,8 @@ MENU ReadyGame()
 	int input = 0;
 	while (true)
 	{
-		DrawReadyGame();
-		DrawUserCursor(y);
+		draw.DrawReadyGame();
+		draw.DrawUserCursor(y);
 		input = _getch();
 		//→←↑↓
 		if (input == MAGIC_KEY1)
@@ -194,7 +100,7 @@ MENU ReadyGame()
 void InfoGame()
 {
 	int input = 0;
-	DrawInfoGame();
+	draw.DrawInfoGame();
 	while (true)
 	{
 		input = _getch();
@@ -305,7 +211,7 @@ void StartGame()
 		while (true)
 		{
 			//1문제를 가지고 문제를 푼다.
-			DrawStartGame(life, score, questionStr, answerStr);
+			draw.DrawStartGame(life, score, questionStr, answerStr);
 
 			if (life == 0)
 			{
@@ -313,7 +219,7 @@ void StartGame()
 				endTime = clock();
 				int playTime = static_cast<int>((endTime - startTime) / CLOCKS_PER_SEC);
 
-				DrawGameOver(playTime);
+				draw.DrawGameOver(playTime);
 				int input = 0;
 				while (true)
 				{
