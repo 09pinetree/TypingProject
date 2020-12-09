e9e900c86683223983ae7c27755787d2100684bf
Kwonhyukjun <book9610@naver.com>
2020-12-10 00:02:51 +0900

    addad 3 poems

---
+++ Ubuntu_test/wordspawn.h
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string PoemAList(int a){
+	switch(a){
+		case 1: return "The Road Not Taken"; break;
+		case 2: return "by Robert Frost"; break;
+		case 3: return "Two roads diverged in a yellow wood,"; break;
+		case 4: return "And sorry I could not travel both"; break;
+		case 5: return "And be one traveler, long I stood"; break;
+		case 6: return "And looked down one as far as I could"; break;
+		case 7: return "To where it bent in the undergrowth"; break;
+		case 8: return "Then took the other, as just as fair,"; break;
+		case 9: return "And having perhaps the better claim,"; break;
+		case 10: return "Because it was grassy and wanted wear;"; break;
+		case 11: return "Though as for that the passing there"; break;
+		case 12: return "Had worn them really about the same,"; break;
+		case 13: return "And both that morning equally lay"; break;
+		case 14: return "In leaves no step had trodden black."; break;
+		case 15: return "Oh, I kept the first for another day!"; break;
+		case 16: return "Yet knowing how way leads on to way,"; break;
+		case 17: return " doubted if I should ever come back."; break;
+		case 18: return "I shall be telling this with a sigh"; break;
+		case 19: return "Somewhere ages and ages hence"; break;
+		case 20: return "Two roads diverged in a wood, and I"; break;
+		case 21: return "I took the one less traveled by,"; break;
+		case 22: return "And that has made all the difference."; break;
+		default: return "Default"; break;
+	}
+}
+
+string PoemBList(int a){
+	switch(a){
+		case 1: return"Still I Rise"; break;
+		case 2: return"BY Maya Angelou"; break;
+		case 3: return"You may write me down in history"; break;
+		case 4: return"With your bitter, twisted lies,"; break;
+		case 5: return"You may trod me in the very dirt"; break;
+		case 6: return"But still, like dust, I'll rise."; break;
+		case 7: return"Does my sassiness upset you?"; break;
+		case 8: return"Why are you beset with gloom?"; break;
+		case 9: return"’Cause I walk like I've got oil wells"; break;
+		case 10: return"Pumping in my living room."; break;
+		case 11: return"Just like moons and like suns,"; break;
+		case 12: return"With the certainty of tides,"; break;
+		case 13: return"Just like hopes springing high,"; break;
+		case 14: return"Still I'll rise."; break;
+		case 15: return"Did you want to see me broken?"; break;
+		case 16: return"Bowed head and lowered eyes?"; break;
+		case 17: return"Shoulders falling down like teardrops,"; break;
+		case 18: return"Weakened by my soulful cries?"; break;
+		case 19: return"Does my haughtiness offend you?"; break;
+		case 20: return"Don't you take it awful hard"; break;
+		case 21: return"’Cause I laugh like I've got gold mines"; break;
+		case 22: return"Diggin’ in my own backyard."; break;
+		case 23: return"You may shoot me with your words,"; break;
+		case 24: return"You may cut me with your eyes,"; break;
+		case 25: return"You may kill me with your hatefulness,	"; break;
+		case 26: return"But still, like air, I’ll rise."; break;
+		case 27: return"Does my sexiness upset you?"; break;
+		case 28: return"Does it come as a surprise"; break;
+		case 29: return"That I dance like I've got diamonds"; break;
+		case 30: return"At the meeting of my thighs?"; break;
+		case 31: return"Out of the huts of history’s shame"; break;
+		case 32: return"I rise"; break;
+		case 33: return"Up from a past that’s rooted in pain"; break;
+		case 34: return"I rise"; break;
+		case 35: return"I'm a black ocean, leaping and wide,"; break;
+		case 36: return"Welling and swelling I bear in the tide."; break;
+		case 37: return"Leaving behind nights of terror and fear"; break;
+		case 38: return"I rise"; break;
+		case 39: return"Into a daybreak that’s wondrously clear"; break;
+		case 40: return"I rise"; break;
+		case 41: return"Bringing the gifts that my ancestors gave,"; break;
+		case 42: return"I am the dream and the hope of the slave."; break;
+		case 43: return"I rise"; break;
+		case 44: return"I rise"; break;
+		case 45: return"I rise."; break;
+		default: return "Default"; break;
+	}
+}
+
+string PoemCList(int a){
+	switch(a){
+		case 1: return "Daddy"; break;
+		case 2: return "By Sylbia Plath"; break;
+		case 3: return "You do not do, you do not do"; break;
+		case 4: return "Any more, black shoe"; break;
+		case 5: return "In which I have lived like a foot"; break;
+		case 6: return "For thirty years, poor and white,"; break;
+		case 7: return "Barely daring to breathe or Achoo."; break;
+		case 8: return "Daddy, I have had to kill you."; break;
+		case 9: return "You died before I had time"; break;
+		case 10: return "Marble-heavy, a bag full of God,"; break;
+		case 11: return "Ghastly statue with one gray toe"; break;
+		case 12: return "Big as a Frisco seal	"; break;
+		case 13: return "And a head in the freakish Atlantic"; break;
+		case 14: return "Where it pours bean green over blue"; break;
+		case 15: return "In the waters off beautiful Nauset."; break;
+		case 16: return "I used to pray to recover you."; break;
+		case 17: return "Ach, du."; break;
+		case 18: return "In the German tongue, in the Polish town"; break;
+		case 19: return "Scraped flat by the roller"; break;
+		case 20: return "Of wars, wars, wars."; break;
+		case 21: return "But the name of the town is common."; break;
+		case 22: return "My Polack friend"; break;
+		case 23: return "Says there are a dozen or two."; break;
+		case 24: return "So I never could tell where you"; break;
+		case 25: return "Put your foot, your root,"; break;
+		case 26: return "I never could talk to you."; break;
+		case 27: return "The tongue stuck in my jaw."; break;
+		case 28: return "It stuck in a barb wire snare."; break;
+		case 29: return "Ich, ich, ich, ich,"; break;
+		case 30: return "I could hardly speak."; break;
+		case 31: return "I thought every German was you."; break;
+		case 32: return "And the language obscene"; break;
+		case 33: return "An engine, an engine"; break;
+		case 34: return "Chuffing me off like a Jew."; break;
+		case 35: return "A Jew to Dachau, Auschwitz, Belsen."; break;
+		case 36: return "I began to talk like a Jew."; break;
+		case 37: return "I think I may well be a Jew."; break;
+		case 38: return "The snows of the Tyrol, the clear beer of Vienna"; break;
+		case 39: return "Are not very pure or true."; break;
+		case 40: return "With my gipsy ancestress and my weird luck   "; break;
+		case 41: return "And my Taroc pack and my Taroc pack"; break;
+		case 42: return "I may be a bit of a Jew."; break;
+		case 43: return "I have always been scared of you,"; break;
+		case 44: return "With your Luftwaffe, your gobbledygoo."; break;
+		case 45: return "And your neat mustache"; break;
+		case 46: return "And your Aryan eye, bright blue."; break;
+		case 47: return "Panzer-man, panzer-man, O You"; break;
+		case 48: return "Not God but a swastika"; break;
+		case 49: return "So black no sky could squeak through."; break;
+	}
+}
